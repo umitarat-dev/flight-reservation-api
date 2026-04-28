632a1f5e35eb67a994de5658a30b8f70a7783929
Ümit Arat <umitarat8098@gmail.com>
2026-04-28 16:02:24 +0300

    fix: enforce https and secure proxy headers for swagger

--- main/settings/base.py
+++ main/settings/base.py
@@ -200,6 +200,7 @@ LOGGING = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ]
 }
 
